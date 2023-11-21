3567f8cb1184ec4fcfff8ef73a16c7dbded80d79
AndersonSheep <dinho1994@gmail.com>
2023-11-21 17:07:55 -0300

    select term and prefix

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World, my name is video make bot VMO")
+def askAndReturnSearchTerm():
+    return input("Type a Wikipedia search term: ")
+
+def askAndReturnPrefix():
+    prefixes = ['Who is', 'What is', 'The history of']
+    for i, prefix in enumerate(prefixes):
+        print(f"[{i}] {prefix}")
+    selectIndex = input("Select the number of prefix: ")
+    return prefixes[int(selectIndex)]
+
+def start():
+    content = {}
+    content["searchTerm"] = askAndReturnSearchTerm()
+    content["prefix"] = askAndReturnPrefix()
+    print(content)
+
+start()
